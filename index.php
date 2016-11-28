20ee0ad1815442cccac0669b7fffffcc23c368e3
elle <svilena_koleva@yahoo.com>
2016-11-28 20:16:23 +0100

    new menu implementation
    two-line drop down

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!doctype html>
 <html>
 <head>
-<link rel="stylesheet" href="css/styles.css">
+
 <meta charset="utf-8">
 <title>Copenhagen Limousine Service</title>
 <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-<link href='http://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
 </head>
 
 <body>
--- menu.php
+++ menu.php
@@ -3,19 +3,55 @@
 $curpage = basename ($_SERVER['PHP_SELF']);
 ?>
 
-    <div id='main-nav'>
-        <ul>
-            <li <?php if($curpage == 'index.php') {echo 'class="active"'; } ?>><a href="index.php">Home</a></li>
-            <li <?php if($curpage == 'gallery.php') {echo 'class="active"'; } ?>><a href="gallery.php">Gallery</a></li>
-            <li <?php if($curpage == 'about.php') {echo 'class="active"'; } ?>><a href="about.php" >About<span class="drop-down"></a>
-                <ul>
-                  <li><a href="#">Island Info</a></li>
-                  <li><a href="#">Native Culture</a></li>
-                  <li><a href="#">Wildlife</a></li>
-                </ul>
+    <div class="css_menu_two_line">
+        <ul class="two_line_menu">
+            <li <?php if($curpage == 'index.php') {echo 'class="active"'; } ?>><a href="index.php">Home</a>
+               
             </li>
-            <li <?php if($curpage == 'login.php') {echo 'class="active"'; } ?>><a href="login.php" >Login</a></li>
-            <li <?php if($curpage == 'register.php') {echo 'class="active"'; } ?>><a href="register.php" >Register</a></li>
+            
+            <li <?php if($curpage == 'about.php') {echo 'class="active"'; } ?>><a href="about.php" >About<span class="drop-down"></a>
+            	<ul>
+                    <li><a href="#">The Company</a></li>
+                    <li><a href="#">Our History</a></li>
+                    <li><a href="#">Archive</a></li>
+           		</ul>
+            </li>
+            
+            <li <?php if($curpage == 'services.php') {echo 'class="active"'; } ?>><a href="services.php">Services</a>
+           		<ul>
+                    <li><a href="#">Road Show</a></li>
+                    <li><a href="#">Transfer Services</a></li>
+                    <li><a href="#">Pendler Services</a></li>
+                    <li><a href="#">Crew Transportation</a></li>
+                    <li><a href="#">Worldwide Limousine Services</a></li>
+                    <li><a href="#">Guardian Angels</a></li>
+                    <li><a href="#">Security &amp; Bodyguards</a></li>
+                    <li><a href="#">Use Your Transport Time</a></li>
+                    <li><a href="#">Sightseeing</a></li>
+            	</ul> 
+            
+           </li>
+            
+            <li <?php if($curpage == 'cars.php') {echo 'class="active"'; } ?>><a href="cars.php">Cars</a>
+            	 <ul>
+                    <li><a href="#">Mercedes Benz E-class</a></li>
+                    <li><a href="#">Mercedes Benz S-class</a></li>
+                    <li><a href="#">VW Multivan</a></li>
+                    <li><a href="#">Mercedes Benz Viano</a></li>
+                    <li><a href="#">Mercedes Sprinter Minibus</a></li>
+            	</ul>
+            </li>
+            
+            <li <?php if($curpage == 'login.php') {echo 'class="active"'; } ?>><a href="login.php">Booking</a></li>
+            
+            <li <?php if($curpage == 'news.php') {echo 'class="active"'; } ?>><a href="news.php">News</a>
+            <ul>
+                <li><a href="#">Articles</a></li>
+                <li><a href="#">Our App</a></li>
+            </ul>
+            </li> 
+            <li <?php if($curpage == 'contact.php') {echo 'class="active"'; } ?>><a href="contact.php">Contact</a></li>
+            
         </ul>
     </div>
   
