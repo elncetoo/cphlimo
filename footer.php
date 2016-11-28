4496349e87443305936fcfba25d616066777556f
elle <svilena_koleva@yahoo.com>
2016-11-28 23:39:04 +0100

    testimonials

--- footer.php
+++ footer.php
@@ -1,7 +1,11 @@
 <link rel="stylesheet" href="assets/css/footer.css">
+ 
 <div class="footer">
+ </br>
 <p>Student Project <a style="text-decoration:none; color: #6b6b6b;" href="www.cphbusiness.dk">CPH Business</a> &copy; 2016</p>
+</br>
 </div>
+</br>
 </body>
 </html>
 
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
      <title>Copenhagen Limousine Service</title>
@@ -11,10 +11,11 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
         <link href="assets/css/style.css" rel="stylesheet" type="text/css" >
         <link rel="stylesheet" href="assets/css/slider-style.css">
+        <link rel="stylesheet" href="assets/css/carousel-style.css">
         <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
  </head>
     
-    <body>
+    <body style="background-color: #000000;">
      <?php include 'menu.php';?>
     
     
@@ -24,9 +25,8 @@
         </br>
         </br>
         
-        <body>
-          <div class="container">
-          <h1>Browse through our services</h1>
+        <div class="container">
+          <h1>Our Services</h1>
           <div class="row">
             <div class="col-md-12">
               <div class="carousel slide multi-item-carousel" id="theCarousel">
@@ -63,17 +63,126 @@
               </div>
             </div>
           </div>
-        </div>
+        
             
   		</br>
         </br>
         </br>
         </br>
         
-
+         <div style="background-color: #000000;" >
+          <div class="row">
+            <div class='col-md-offset-2 col-md-8 text-center'>
+            <h2>What our customers are saying..</h2>
+            </div>
+          </div>
+          <div class='row'>
+            <div class='col-md-offset-2 col-md-8'>
+              <div class="carousel slide" data-ride="carousel" id="quote-carousel">
+                <!-- Bottom Carousel Indicators -->
+                <ol class="carousel-indicators">
+                  <li data-target="#quote-carousel" data-slide-to="0" class="active"></li>
+                  <li data-target="#quote-carousel" data-slide-to="1"></li>
+                  <li data-target="#quote-carousel" data-slide-to="2"></li>
+                </ol>
+                
+                <!-- Carousel Slides / Quotes -->
+                <div class="carousel-inner">
+                
+                  <!-- Quote 1 -->
+                  <div class="item active">
+                    <blockquote>
+                      <div class="row">
+                        <div class="col-sm-3 text-center">
+                          <img class="img-circle" src="http://www.reactiongifs.com/r/overbite.gif" style="width: 100px;height:100px;">
+                          <!--<img class="img-circle" src="https://s3.amazonaws.com/uifaces/faces/twitter/kolage/128.jpg" style="width: 100px;height:100px;">-->
+                        </div>
+                        <div class="col-sm-9">
+                          <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit!</p>
+                          <small>Someone famous</small>
+                        </div>
+                      </div>
+                    </blockquote>
+                  </div>
+                  <!-- Quote 2 -->
+                  <div class="item">
+                    <blockquote>
+                      <div class="row">
+                        <div class="col-sm-3 text-center">
+                          <img class="img-circle" src="https://s3.amazonaws.com/uifaces/faces/twitter/mijustin/128.jpg" style="width: 100px;height:100px;">
+                        </div>
+                        <div class="col-sm-9">
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam auctor nec lacus ut tempor. Mauris.</p>
+                          <small>Someone famous</small>
+                        </div>
+                      </div>
+                    </blockquote>
+                  </div>
+                  <!-- Quote 3 -->
+                  <div class="item">
+                    <blockquote>
+                      <div class="row">
+                        <div class="col-sm-3 text-center">
+                          <img class="img-circle" src="https://s3.amazonaws.com/uifaces/faces/twitter/keizgoesboom/128.jpg" style="width: 100px;height:100px;">
+                        </div>
+                        <div class="col-sm-9">
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rutrum elit in arcu blandit, eget pretium nisl accumsan. Sed ultricies commodo tortor, eu pretium mauris.</p>
+                          <small>Someone famous</small>
+                        </div>
+                      </div>
+                    </blockquote>
+                  </div>
+                </div
+              ></div>                          
+            </div>
+          </div>
+        </div>
+        </br>
+        </br>
+        </br>
+      </div><!--containerends here--> 
+      </br>
+      </br>
 	<?php require 'footer.php';?>
 
  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script src="assets/js/slider-index.js"></script>
+  <script src="assets/js/carousel-index.js"></script>
+	<script>
+$(document).ready(function(){
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+  
+  $(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+          $(this).addClass("slide");
+        }
+    });
+  });
+})
+</script>
