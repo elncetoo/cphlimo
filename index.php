048b0d8df634ee9894d83ffc6006efa78e5d3819
elle <svilena_koleva@yahoo.com>
2016-11-28 22:22:43 +0100

    slider carouse implemented
    seviseconthe home page

--- index.php
+++ index.php
@@ -7,9 +7,10 @@
       <meta name="description" content="Copenhagen Limousine Service web page">
     
         <!-- Latest compiled and minified CSS -->
-        <link href="css/bootstrap.css" rel="stylesheet">
-        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
         <link href="assets/css/style.css" rel="stylesheet" type="text/css" >
+        <link rel="stylesheet" href="assets/css/slider-style.css">
         <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
  </head>
     
@@ -22,12 +23,57 @@
           </article>
         </br>
         </br>
-    
-
+        
+        <body>
+          <div class="container">
+          <h1>Browse through our services</h1>
+          <div class="row">
+            <div class="col-md-12">
+              <div class="carousel slide multi-item-carousel" id="theCarousel">
+                <div class="carousel-inner">
+                  <div class="item active">
+                    <div class="col-xs-4"><a href="#1"><img src="http://placehold.it/300/f44336/000000" class="img-responsive"></a></div>
+                  </div>
+                  <div class="item">
+                    <div class="col-xs-4"><a href="#1"><img src="http://placehold.it/300/e91e63/000000" class="img-responsive"></a></div>
+                  </div>
+                  <div class="item">
+                    <div class="col-xs-4"><a href="#1"><img src="http://placehold.it/300/9c27b0/000000" class="img-responsive"></a></div>
+                  </div>
+                  <div class="item">
+                    <div class="col-xs-4"><a href="#1"><img src="http://placehold.it/300/673ab7/000000" class="img-responsive"></a></div>
+                  </div>
+                  <div class="item">
+                    <div class="col-xs-4"><a href="#1"><img src="http://placehold.it/300/4caf50/000000" class="img-responsive"></a></div>
+                  </div>
+                  <div class="item">
+                    <div class="col-xs-4"><a href="#1"><img src="http://placehold.it/300/8bc34a/000000" class="img-responsive"></a></div>
+                  </div>
+                  <!-- add  more items here -->
+                  <!-- Example item start:  -->
+                  
+                  <div class="item">
+                    <div class="col-xs-4"><a href="#1"><img src="http://placehold.it/300/8bc34a/000000" class="img-responsive"></a></div>
+                  </div>
+                  
+                  <!--  Example item end -->
+                </div>
+                 <a class="left carousel-control" href="#theCarousel" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
+        		 <a class="right carousel-control" href="#theCarousel" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+            
+  		</br>
+        </br>
+        </br>
+        </br>
+        
 
 	<?php require 'footer.php';?>
 
  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
- <!-- Include all compiled plugins (below), or include individual files as needed -->
- <script src="assets/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="assets/js/slider-index.js"></script>
