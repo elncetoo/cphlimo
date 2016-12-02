04149ff139a2fea54e4aa5fe9ae1be7a940a0773
elle <svilena_koleva@yahoo.com>
2016-12-02 18:08:09 +0100

    cars
    +modals

--- cars.php
+++ cars.php
@@ -20,13 +20,12 @@
 		<div class="col-lg-12 text-center">
 			<?php include 'menu.php';?>
         </div>
-		
 		<section id="portfolio">
          <div class="container">
 			<div class="row">
             
 				<div class="col-lg-12 text-center">
-          <div class="section-title">
+                  <div class="section-title">
                   		
                         <br>
 						<h2>Our Cars</h2>
@@ -34,17 +33,22 @@
                         <br>
                         <br>
                         <br>
-					</div>
+                        <h4 style="padding-left:5%; color:#A7A7A7;">Quality Driving. We have a wide selection of vehicles at your disposal. We drive only the safest, most comfortable cars.</h4>
+                        <h4 style="padding-left:5%; color:#A7A7A7;">For occasions demanding several vehicles, we are pleased that we are able to provide several high-end luxury or business</h4>
+                        <h4 style="padding-left:5%; color:#A7A7A7;">vehicles that are completely identical, thus giving your venue to most professional appearance.</h4>
+						<br>
+                        <br>
+                    </div>
 				</div>
 			</div>
-			<div class="row row-0-gutter" > <!-- start row 1 -->
+			<div class="row row-0-gutter">
 				<!-- start portfolio item -->
-				<div class="col-md-6 col-0-gutter" >
+				<div class="col-md-6 col-0-gutter">
 					<div class="ot-portfolio-item">
 						<figure class="effect-bubba">
 							<img src="http://farm3.static.flickr.com/2756/4178822453_1c986319a8_o.jpg" alt="img02" class="img-responsive" />
 							<figcaption>
-							<br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+								<br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
 								<p>View more</p>
 								<a href="#" data-toggle="modal" data-target="#Modal-1"></a>
 							</figcaption>
@@ -54,33 +58,33 @@
 				<!-- end portfolio item -->
 				<!-- start portfolio item -->
 				<div class="col-md-6 text-center">
-					<div class="ot-portfolio-item"> <br><br><br><br><br><br>
+					<div class="ot-portfolio-item"> <br><br><br><br><br><br> <br>
 						<h4 class="carh">Mercedes Benz E-class</h4> <br>
 						<img class="carextras" src="img/s1.png" alt="car seats" width="10%">
 						<img class="carextras" src="img/l1.png" alt="luggage" width="10%">
 						<img class="carextras" src="img/air.png" alt="air conditioning" width="10%">
 						<img class="carextras" src="img/gpss.png" alt="gps" width="10%">
-					</div>
+                    </div>
 				</div>
 				<!-- end portfolio item -->
-			</div><!--end row 1-->
-			<div class="row row-0-gutter"> <!-- start row 2 -->
+              </div><!--end row 1-->
+              <div class="row row-0-gutter">
 				<!-- start portfolio item -->
-				<div class="col-md-6 text-center">
-					<div class="ot-portfolio-item"> <br><br><br><br><br><br>
+				 <div class="col-md-6 text-center">
+					<div class="ot-portfolio-item"> <br><br><br><br><br><br> <br>
 						<h4 class="carh">Mercedes Benz S-class</h4> <br>
 						<img class="carextras" src="img/s2.png" alt="car seats" width="10%">
 						<img class="carextras" src="img/l2.png" alt="luggage" width="10%">
 						<img class="carextras" src="img/air.png" alt="air conditioning" width="10%">
 						<img class="carextras" src="img/gpss.png" alt="gps" width="10%">
-					</div>
+                    </div>
 				</div>
-				<!-- end portfolio item -->
-				<!-- start portfolio item -->
-				<div class="col-md-6 col-0-gutter">
+                <!-- end portfolio item -->
+                 <!-- start portfolio item -->
+                  <div class="col-md-6 col-0-gutter">
 					<div class="ot-portfolio-item">
 						<figure class="effect-bubba">
-							<img src="img/portfolio-1.jpg" alt="img02" class="img-responsive"/>
+							<img src="http://image.motortrend.com/f/53228402/Mercedes-Benz-S-Class-Coupe-Concept-front-end.jpg" alt="img02" class="img-responsive" />
 							<figcaption>
 								<br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
 								<p>View more</p>
@@ -90,13 +94,13 @@
 					</div>
 				</div>
 				<!-- end portfolio item -->
-			</div><!--end row 2-->
-			<div class="row row-0-gutter" > <!-- start row 3 -->
+              </div><!--end row 2-->
+			  <div class="row row-0-gutter">
 				<!-- start portfolio item -->
-				<div class="col-md-6 col-0-gutter" >
+				<div class="col-md-6 col-0-gutter">
 					<div class="ot-portfolio-item">
 						<figure class="effect-bubba">
-							<img src="img/portfolio-1.jpg" alt="img02" class="img-responsive" />
+							<img src="http://cdn.pinthiscars.com/images/vw-caravelle-8.jpg" alt="img02" class="img-responsive" />
 							<figcaption>
 								<br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
 								<p>View more</p>
@@ -108,51 +112,53 @@
 				<!-- end portfolio item -->
 				<!-- start portfolio item -->
 				<div class="col-md-6 text-center">
-					<div class="ot-portfolio-item"> <br><br><br><br><br><br>
+					<div class="ot-portfolio-item"> <br><br><br><br><br><br> <br>
 						<h4 class="carh">VW Multivan</h4> <br>
 						<img class="carextras" src="img/s3.png" alt="car seats" width="10%">
 						<img class="carextras" src="img/l3.png" alt="luggage" width="10%">
 						<img class="carextras" src="img/air.png" alt="air conditioning" width="10%">
 						<img class="carextras" src="img/gpss.png" alt="gps" width="10%">
-					</div>
+                    </div>
 				</div>
 				<!-- end portfolio item -->
-			</div><!--end row 3-->
-			<div class="row row-0-gutter"> <!-- start row 4 -->
+              </div><!--end row 3-->
+              
+          <div class="row row-0-gutter">
 				<!-- start portfolio item -->
 				<div class="col-md-6 text-center">
-					<div class="ot-portfolio-item"> <br><br><br><br><br><br>
-						<h4 class="carh">Mercedes Benz Viano</h4><br>
+					<div class="ot-portfolio-item"> <br><br><br><br><br><br> <br>
+						<h4 class="carh">Mercedes Benz Viano</h4> <br>
 						<img class="carextras" src="img/s5.png" alt="car seats" width="10%">
 						<img class="carextras" src="img/l5.png" alt="luggage" width="10%">
 						<img class="carextras" src="img/air.png" alt="air conditioning" width="10%">
 						<img class="carextras" src="img/gpss.png" alt="gps" width="10%">
-					</div>
+                    </div>
 				</div>
 				<!-- end portfolio item -->
-				<!-- start portfolio item -->
+                <!-- start portfolio item -->
 				<div class="col-md-6 col-0-gutter">
 					<div class="ot-portfolio-item">
 						<figure class="effect-bubba">
-							<img src="img/portfolio-1.jpg" alt="Mercedes Benz V" class="img-responsive" />
+							<img src="http://media.emercedesbenz.com.s3.amazonaws.com/magazine/wp-content/uploads/Mercedes-Benz-Viano-821424_1514972_2480_1748_11C157_01.jpg" alt="img02" class="img-responsive" />
 							<figcaption>
 								<br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
-                                <p>View more</p>
+								<p>View more</p>
 								<a href="#" data-toggle="modal" data-target="#Modal-4"></a>
 							</figcaption>
 						</figure>
 					</div>
 				</div>
 				<!-- end portfolio item -->
-			</div><!--end row 4-->
-			<div class="row row-0-gutter"> <!-- start row 5 -->
+              </div><!--end row 4-->
+              
+              <div class="row row-0-gutter">
 				<!-- start portfolio item -->
 				<div class="col-md-6 col-0-gutter">
 					<div class="ot-portfolio-item">
 						<figure class="effect-bubba">
-							<img src="img/portfolio-1.jpg" alt="img02" class="img-responsive" />
+							<img src="http://nebula.wsimg.com/eb6adb1fd6644f11d22e48c1c720544d?AccessKeyId=330A5EE49AF214881DE0&disposition=0" alt="img02" class="img-responsive" />
 							<figcaption>
-								<br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+								<br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> 
 								<p>View more</p>
 								<a href="#" data-toggle="modal" data-target="#Modal-5"></a>
 							</figcaption>
@@ -162,15 +168,134 @@
 				<!-- end portfolio item -->
 				<!-- start portfolio item -->
 				<div class="col-md-6 text-center">
-					<div class="ot-portfolio-item"> <br><br><br><br><br><br>
+					<div class="ot-portfolio-item"> <br><br><br><br><br><br> <br>
 						<h4 class="carh">Mercedes Sprinter Minibus</h4> <br>
 						<img class="carextras" src="img/s4.png" alt="car seats" width="10%">
 						<img class="carextras" src="img/l2.png" alt="luggage" width="10%">
 						<img class="carextras" src="img/air.png" alt="air conditioning" width="10%">
 						<img class="carextras" src="img/gpss.png" alt="gps" width="10%">
-					</div>
+                    </div>
 				</div>
 				<!-- end portfolio item -->
-			</div><!--end row 5-->
-	</body>
-</html>
+              </div><!--end row 5-->
+		</div><!-- container -->
+		</section>
+
+		
+		<p id="back-top">
+			<a href="#top"><i class="fa fa-angle-up"></i></a>
+		</p>
+		
+
+		<!-- Modal for portfolio item 1 -->
+		<div class="modal fade" id="Modal-1" tabindex="-1" role="dialog" aria-labelledby="Modal-label-1">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						<h4 class="modal-title" id="Modal-label-1">Mercedes Benz E-class</h4>
+					</div>
+					<div class="modal-body">
+						<img src="http://farm3.static.flickr.com/2756/4178822453_1c986319a8_o.jpg" alt="img01" class="img-responsive" />
+						<div class="modal-works"><span>Denmark</span></div>
+						<p style="padding:4%;">Smooth, comforting, secure and superior, the E-Class Saloon is the pinnacle of effortless driving. This saloons interior and exterior exemplifies sophistication and refinement to make you feel even more pampered on your journey. You will find the latest Mercedes technology on every aspect of the E-Class Saloon.</p>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- Modal for portfolio item 2 -->
+		<div class="modal fade" id="Modal-2" tabindex="-1" role="dialog" aria-labelledby="Modal-label-2">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						<h4 class="modal-title" id="Modal-label-2">Mercedes Benz S-class</h4>
+					</div>
+					<div class="modal-body">
+						<img src="http://image.motortrend.com/f/53228402/Mercedes-Benz-S-Class-Coupe-Concept-front-end.jpg" alt="img01" class="img-responsive" />
+						<div class="modal-works"><span>Denmark</span></div>
+						<p style="padding:4%;">The S-Class is the ultimate luxury saloon. Repeatedly referred to as the "best car in the world" by motoring magazines, it is sumptuous and elegant while offering visionary technological innovations. Driving it feels like flying first class.</p>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- Modal for portfolio item 3 -->
+		<div class="modal fade" id="Modal-3" tabindex="-1" role="dialog" aria-labelledby="Modal-label-3">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						<h4 class="modal-title" id="Modal-label-3">VW Multivan</h4>
+					</div>
+					<div class="modal-body">
+						<img src="http://cdn.pinthiscars.com/images/vw-caravelle-8.jpg" alt="img01" class="img-responsive" />
+						<div class="modal-works"><span>Denmark</span></div>
+						<p style="padding:4%;">For roadshows and business transport for a number of passengers less than 6. This car says full professionalism and gives a great level of comfort and opportunity for meetings during the drive.</p>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- Modal for portfolio item 4 -->
+		<div class="modal fade" id="Modal-4" tabindex="-1" role="dialog" aria-labelledby="Modal-label-4">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						<h4 class="modal-title" id="Modal-label-4">Mercedes Benz Viano</h4>
+					</div>
+					<div class="modal-body">
+						<img src="http://media.emercedesbenz.com.s3.amazonaws.com/magazine/wp-content/uploads/Mercedes-Benz-Viano-821424_1514972_2480_1748_11C157_01.jpg" alt="img01" class="img-responsive" />
+						<div class="modal-works"><span>Denmark</span></div>
+						<p style="padding:4%;">As the Mercedes Benz Vito, just bigger and new. This car is the future within vans for the picky. We expect this will be the clear choice for everyone, as soon as they have tried it.</p>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- Modal for portfolio item 5 -->
+		<div class="modal fade" id="Modal-5" tabindex="-1" role="dialog" aria-labelledby="Modal-label-5">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						<h4 class="modal-title" id="Modal-label-5">Mercedes Sprinter Minibus</h4>
+					</div>
+					<div class="modal-body">
+						<img src="http://nebula.wsimg.com/eb6adb1fd6644f11d22e48c1c720544d?AccessKeyId=330A5EE49AF214881DE0&disposition=0" alt="img01" class="img-responsive" />
+						<div class="modal-works"><span>Denmark</span></div>
+						<p style="padding:4%;">For a majority of transportation solutions for transporting groups of up to 15 passengers (excluding driver) per vehicle, we have chosen the Mercedes Sprinter, which in our fleet is a slightly extended version that offer amble room for passengers as well as luggage. The vehicle has a very smooth and comfortable ride.</p>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					</div>
+				</div>
+			</div>
+		</div>
+        
+   		 <?php require 'footer.php';?>
+         <br>
+         <br>
+        
+		<!-- Bootstrap core JavaScript
+			================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+		<script src="assets/js/SmoothScroll.js"></script>
+		<script src="assets/js/theme-scripts.js"></script>
+	
